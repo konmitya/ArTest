4a63727ddff48e8871d6fac3b6355651eae02f4f
konmitya <konmitya@yandex.ru>
2024-08-01 09:25:08 +0300

    Тест

--- ArTest.py
+++ ArTest.py
@@ -1,65 +1,18 @@
 import streamlit as st
-import hashlib
-import sqlite3
 
-# Подключение к базе данных SQLite
-conn = sqlite3.connect('articuls.db')
-c = conn.cursor()
- #ffas
-# Создание таблицы, если ее еще нет
-c.execute('''
-CREATE TABLE IF NOT EXISTS articuls (
-    articul TEXT PRIMARY KEY,
-    model_name TEXT UNIQUE
-)
-''')
-conn.commit()
+st.title("Ипотечный калькулятор")
 
+# Ввод стоимости квартиры
+property_value = st.number_input("Стоимость квартиры (в рублях)", min_value=0, value=1000000, step=10000)
 
-def model_to_articul(model_name):
-    # Получить хеш модели
-    hash_object = hashlib.sha256(model_name.encode())
-    hex_dig = hash_object.hexdigest()
+# Ввод первоначального взноса
+down_payment = st.number_input("Первоначальный взнос (в рублях)", min_value=0, value=200000, step=10000)
 
-    # Преобразовать хеш в десятичное число
-    decimal_number = int(hex_dig, 16)
+# Ввод процентной ставки
+interest_rate = st.number_input("Ставка кредита (в %)", min_value=0.0, value=10.0, step=0.1)
 
-    # Взять последние 10 цифр
-    articul = str(decimal_number)[-10:]
+# Ввод срока кредита
+loan_term_years = st.number_input("Срок кредита (в годах)", min_value=1, value=20, step=1)
 
-    return articul
-
-
-def save_articul_model(articul, model_name):
-    c.execute('INSERT OR IGNORE INTO articuls (articul, model_name) VALUES (?, ?)', (articul, model_name))
-    conn.commit()
-
-
-def get_model_by_articul(articul):
-    c.execute('SELECT model_name FROM articuls WHERE articul = ?', (articul,))
-    result = c.fetchone()
-    if result:
-        return result[0]
-    else:
-        return None
-
-
-st.title("Генерация артикулов для насосов CNP")
-
-# Ввод модели и генерация артикула
-model_name = st.text_input("Введите модель насоса для генерации артикула:", "")
-if model_name:
-    articul = model_to_articul(model_name)
-    save_articul_model(articul, model_name)
-    st.write(f"Модель: {model_name}")
-    st.write(f"Артикул: {articul}")
-
-# Обратное преобразование артикула в модель
-input_articul = st.text_input("Введите артикул для поиска модели:", "")
-if input_articul:
-    found_model = get_model_by_articul(input_articul)
-    if found_model:
-        st.write(f"Артикул: {input_articul}")
-        st.write(f"Модель: {found_model}")
-    else:
-        st.write(f"Модель для артикула {input_articul} не найдена.")
+# Ввод типа ежемесячных платежей
+payment_type = st.selectbox("Тип ежемесячных платежей", ["Аннуитетные", "Дифференцированные"])
